4dda504dc583c0e4f58d125febb45013179c5572
christopherlovell <chrisl1991@hotmail.co.uk>
2020-11-05 11:53:01 -0500

    fix spherical coordinate transformation

--- sphviewer/Camera.py
+++ sphviewer/Camera.py
@@ -78,11 +78,13 @@ class Camera(object):
             self.__params[key] = kargs[key]
 
     def _get_camera(self, plane, **kargs):
-        x0 = -self.__params['r']*(np.sin(self.__params['p']*np.pi/180.) *
-                                  np.cos(self.__params['t']*np.pi/180.))
-        z0 = self.__params['r']*(np.cos(self.__params['p']*np.pi/180.) *
-                                 np.cos(self.__params['t']*np.pi/180.))
-        y0 = self.__params['r']*np.sin(self.__params['t']*np.pi/180.)
+        x0 = self.__params['r']*(np.sin(self.__params['t']*np.pi/180.) *
+                                  np.cos(self.__params['p']*np.pi/180.))
+
+        y0 = self.__params['r']*np.sin(self.__params['p']*np.pi/180.)
+        
+        z0 = self.__params['r']*(np.cos(self.__params['t']*np.pi/180.) *
+                                 np.cos(self.__params['p']*np.pi/180.))
 
         xcam = self.__params['x']+x0
         ycam = self.__params['y']+y0
